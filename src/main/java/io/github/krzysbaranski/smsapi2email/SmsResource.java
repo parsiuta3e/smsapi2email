c7b10e911ad0c47467a313b63ee816edbf22497e
Patryk Arsiuta <patryk.arsiuta@3e.pl>
2019-02-18 15:13:12 +0100

    Update SmsResource.java dodanie wsparcia dla metody POST

--- src/main/java/io/github/krzysbaranski/smsapi2email/SmsResource.java
+++ src/main/java/io/github/krzysbaranski/smsapi2email/SmsResource.java
@@ -3,7 +3,7 @@ package io.github.krzysbaranski.smsapi2email;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -19,12 +19,12 @@ public class SmsResource {
     private final MessageGenerator messageGenerator = new MailMessageGenerator(Session.getDefaultInstance(System.getProperties()));
     private final Sender sender = new MailSender();
     /**
-     * Method handling HTTP GET requests. The returned object will be sent
+     * Method handling HTTP POST requests. The returned object will be sent
      * to the client as "text/plain" media type.
      *
      * @return String that will be returned as a text/plain response.
      */
-    @GET
+    @POST
     @Produces(MediaType.TEXT_PLAIN)
     public Response getIt(@QueryParam("username") String username,
                           @QueryParam("password") String password,
